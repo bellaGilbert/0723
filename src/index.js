fcaf8a26b9153fb173de58c78a104cff1df4fa23
bella <903165788@qq.com>
2019-07-23 09:14:50 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React,{Component} from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import store from "./redux/store"
+ReactDOM.render(<App store={store}/>,document.getElementById("root"))
