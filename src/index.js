f6ae0f300f082fb7a5af9f40a5bac88949f29dec
bella <903165788@qq.com>
2019-07-23 09:18:24 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React,{Component} from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import store from "./redux/store"
-ReactDOM.render(<App store={store}/>,document.getElementById("root"))
+ReactDOM.render(<App/>,document.getElementById("root"))
